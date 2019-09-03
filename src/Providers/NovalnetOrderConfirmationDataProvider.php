576754ca76eb03cef608969803be0cef4f59f9e4
NishaJahir <49142761+NishaJahir@users.noreply.github.com>
2019-09-03 13:35:27 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -84,9 +84,6 @@ class NovalnetOrderConfirmationDataProvider
 						$comments .= PHP_EOL . $paymentHelper->getTranslatedText('test_order');
 					}
 					$bank_details = array_merge($db_details, json_decode($invoiceDetails, true));
-					if (in_array($bank_details['paymentName'], ['novalnet_invoice', 'novalnet_prepayment']) && in_array($tid_status, ['91', '100'])) {
-						$comments .= PHP_EOL . $paymentService->getInvoicePrepaymentComments($bank_details);
-					}
 					if(in_array($db_details['payment_id'], ['40','41'])) {
 						$comments .= PHP_EOL . $paymentHelper->getTranslatedText('guarantee_text');
 						if($tid_status == '75' && $db_details['payment_id'] == '41')
@@ -97,6 +94,9 @@ class NovalnetOrderConfirmationDataProvider
 						{
 							$comments .= PHP_EOL . $paymentHelper->getTranslatedText('gurantee_sepa_pending_payment_text');
 						}
+					}
+					if (in_array($bank_details['paymentName'], ['novalnet_invoice', 'novalnet_prepayment']) && in_array($tid_status, ['91', '100'])) {
+						$comments .= PHP_EOL . $paymentService->getInvoicePrepaymentComments($bank_details);
 					}
 					if($db_details['payment_id'] == '59' ) {
 						$comments .= $cashpayment_comments;
